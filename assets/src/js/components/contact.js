e6402f7520b0589adde97c780f6a935d632321b2
jn-prod <nicolas.jouanno@gmail.com>
2019-05-23 17:34:49 +0200

    about update

--- assets/src/js/components/contact.js
+++ assets/src/js/components/contact.js
@@ -1,7 +1,6 @@
 var contact = () => {
   $('.contact').on('click', (e) => {
-    console.log(e.target.innerHTML)
-    e.target.innerHTML = '<a href="mailto:pro@nicolasjouanno.com" class="text-light">pro@nicolasjouanno.com</a>'
+    e.target.innerHTML = '<a href="mailto:pro@nicolasjouanno.com" class="text-primary">pro@nicolasjouanno.com</a>'
   })
 }
 
